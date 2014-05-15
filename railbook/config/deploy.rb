21e9c91d62702dd2bc5c4f4069d41c77aeea34f0
K.Ikeda <ikeda@motionportrait.com>
2014-05-15 11:14:43 +0900

    Modify deploy setting

--- railbook/config/deploy.rb
+++ railbook/config/deploy.rb
@@ -1,15 +1,15 @@
 require 'capistrano/ext/multistage'
 
 set :application, "capistrano sample book"
-set :repository,  "set your repository location here"
+set :repository,  "git://github.com/kazoo/sandbox"
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+# role :web, "your web-server here"                          # Your HTTP server, Apache/etc
+# role :app, "your app-server here"                          # This may be the same as your `Web` server
+# role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
@@ -25,3 +25,18 @@ role :db,  "your slave db-server here"
 #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
 # end
+
+# user settings
+set :user, 'vagrant'
+set :use_sudo, false 
+default_run_options[:pty] = true
+
+def restart_file
+  File.join(current_path, 'tmp', 'restart.txt')
+end
+
+namespace :deploy do
+  task :restart, :roles => :app do
+    run "touch #{restart_file}"
+  end
+end
