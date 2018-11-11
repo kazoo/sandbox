d4774efec7699127775b3a915908b9ed1b243d88
K.Ikeda <kazoo@safie.jp>
2018-11-12 00:16:05 +0900

    modify sample

--- ts-vue/src/views/About.vue
+++ ts-vue/src/views/About.vue
@@ -1,5 +1,63 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about">
+        <h1>hello {{ value | uppercase }}</h1>
+        <input type="text" v-model="testInput">
+        <div>
+            {{ testInput | uppercase }}
+        </div>
+        <button @click="handleClick()">test</button>
+    </div>
 </template>
+
+<script lang="ts">
+import { Component, Prop, Emit, Watch, Vue } from 'vue-property-decorator';
+
+@Component({
+    filters: {
+        uppercase(value: string): string | null {
+            if (!value) {
+                return null;
+            }
+            return value.toUpperCase();
+        }
+    }
+})
+
+export default class HelloTsVue extends Vue {
+    @Prop() val!: string;
+
+    value: string = this.val;
+    testInput: string = '';
+
+    @Watch('value')
+    onValueChange(newVal: string, oldVal: string): void {
+        console.log(`watch: ${newVal}, ${oldVal}`);
+    }
+
+    @Emit('handle-click')
+    clickButton(val: string): void {
+        console.log('button clicked', val);
+    }
+
+    /* lifecycle */
+    created(): void {
+        console.log('created');
+    }
+
+    mounted(): void {
+        console.log('mounted');
+    }
+
+    /* methods */
+    handleClick(): void {
+        if (this.testInput === '') {
+            return;
+        }
+        this.value = this.testInput;
+        this.testInput = '';
+        this.clickButton(this.value);
+    }
+
+}
+
+</script>
